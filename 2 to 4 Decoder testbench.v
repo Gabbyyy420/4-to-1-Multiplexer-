b77da91450cc15a3780eba410c9e41aebe76d584
Gabbyyy420 <89363852+Gabbyyy420@users.noreply.github.com>
2021-12-11 11:25:27 +0800

    Update 2 to 4 Decoder testbench.v

--- 2 to 4 Decoder testbench.v
+++ 2 to 4 Decoder testbench.v
@@ -1,11 +1,11 @@
-module decoder_2_to_4(
-    input a0,
-    input a1,
-    output d0,
-    output d1,
-    output d2,
-    output d3
-    );
-               not(an0,a0),(an1,a1);
-               and(d0,an0,an1),(d1,a0,an1),(d2,an0,a1),(d3,a0,a1);
-endmodule
+initial begin
+// Initialize Inputs
+a0 = 0;a1 = 0;
+// Wait 100 ns for global reset to finish
+#100;
+// Add stimulus here
+#100; a0=1;a1=0;
+#100; a0=0;a1=1;
+#100; a0=1;a1=1;
+
+end
